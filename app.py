c1c786f1cffad64e380677a83dece721a0343ff9
Nathan Sandoval <nathanjs@utexas.edu>
2024-04-07 18:30:47 -0500

    inital heroku

--- app.py
+++ app.py
@@ -257,4 +257,4 @@ def not_found(e):
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000), debug=os.environ.get('DEBUG', 'False') == 'True')
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 80), debug=os.environ.get('DEBUG', 'False') == 'True')
